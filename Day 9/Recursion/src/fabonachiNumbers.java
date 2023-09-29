e114f52f89e0386d74a41032e37c8aebd7aff6f6
Husnain ali <91048404+husnain584@users.noreply.github.com>
2023-09-29 22:53:50 +0500

    Add files via upload

---
+++ Day 9/Recursion/src/fabonachiNumbers.java
@@ -0,0 +1,11 @@
+public class fabonachiNumbers {
+    // find the 5th fabonachi number using recursion;
+    public static void main(String[] args) {
+        int fbnumber = 5;
+
+    }
+    static void recursion(int n){
+        int ans = (n - 1) + ( n + 2);
+    }
+
+}
